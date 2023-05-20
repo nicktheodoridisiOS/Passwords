4ab7c3206160d305828f942dc0a50861997f4a36
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-05-20 15:22:27 +0300

    Create project

---
+++ PasswordGenerator/PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Nick Theodoridis on 20/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
